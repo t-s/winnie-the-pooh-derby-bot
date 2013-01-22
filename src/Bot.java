7a8725f3400cd4196b2de808f9934d93ecd03e1e
Tim Sidie <timothy.a.sidie@nasa.gov>
2013-01-22 16:35:09 +0000

    Named constants.

--- src/Bot.java
+++ src/Bot.java
@@ -15,33 +15,40 @@ public class Bot {
 		int x = 0;
 		int y = 0;
 		int color = 0;
-		int r = 0;
-		int g = 0;
-		int b = 0;
-		int j = 0;
+		int WHITE = 240;
+		
+		int HEIGHT_OF_AREA = 250;
+		int WIDTH_OF_AREA = 250;
+		int TOP_LEFT_X = 874;
+		int TOP_LEFT_Y = 550;
+		
+		int HR_ZONE = 105;
+		int SAFE_ZONE = 50;
+		
 		boolean once = false;
 		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
 		GraphicsDevice devices[] = ge.getScreenDevices();
 
 	    Robot robot = new Robot(devices[1]); 
-	    //robot.mouseMove(950, 700); 
 	    robot.mouseMove(1200,800);
-	    Rectangle screenRect = new Rectangle(874,550,149,250);
+	    Rectangle screenRect = new Rectangle(TOP_LEFT_X,TOP_LEFT_Y,WIDTH_OF_AREA,HEIGHT_OF_AREA);
 	    System.out.println(InputEvent.BUTTON1_MASK);
 	    while(i < 100000)
 	    {
 	    	BufferedImage img = robot.createScreenCapture(screenRect);
-	    	for(x = 0; x < 149; ++x)
+	    	for(x = 0; x < WIDTH_OF_AREA; ++x)
 	    	{
-	    		for(y = 0; y < 250; ++y)
+	    		for(y = 0; y < HEIGHT_OF_AREA; ++y)
 	    		{
 	    			color = img.getRGB(x,y);
 						
-	    			if ((((color)&0xFF) > 240) && (((color>>8)&0xFF) > 240) && (((color>>16)&0xFF) > 240))
+	    			if ((((color)&0xFF) > WHITE) && (((color>>8)&0xFF) > WHITE) && (((color>>16)&0xFF) > WHITE))
 	    			{
-	    				robot.mouseMove(x+874, 550+150);
+	    				//keep Y centered so that Pooh only moves left to right.
+	    				robot.mouseMove(x+TOP_LEFT_X, TOP_LEFT_Y+150);
+	    				
 	    				i++;
-	    				if(y > 105)
+	    				if(y > HR_ZONE)
 	    				{
 	    					robot.mousePress(InputEvent.BUTTON1_MASK);
 	    					
@@ -55,22 +62,28 @@ public class Bot {
 	    							th.sleep(500);
 	    						}
 	    						once = true;
-	    						robot.mouseMove(874+ 74, 550+150);
+	    						robot.mouseMove(TOP_LEFT_X + 74, TOP_LEFT_Y+150);
 	    					}
 	    				}
-	    				if(y < 50)
+	    				if(y < SAFE_ZONE)
 	    				{
 	    					robot.mouseRelease(InputEvent.BUTTON1_MASK);
 	    					once = false;
 	    				}
-	    				//File outputfile = new File("saved" + i + ".png");
-	    		    	//try {
-	    				//	ImageIO.write(img, "png", outputfile);
-	    					
-	    				//} catch (IOException e) {
-	    				//	e.printStackTrace();
+	    				
+	    				//Uncomment below for image generation of homerun zone - i.e. where bot tracks ball
+	    				
+	    				//if(i%100==0)
+	    				//{
+	    				//	File outputfile = new File("saved" + i/100 + ".png");
+	    				//	try {
+	    				//		ImageIO.write(img, "png", outputfile);
+	    				//	
+	    				//	} catch (IOException e) {
+	    				//		e.printStackTrace();
+	    				//	}
 	    				//}
-	    			}		
+	    			}
 	    		}
 	    	}
 	    }	    	
