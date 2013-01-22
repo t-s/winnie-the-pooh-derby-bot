a2db621aba4a2c9b142afff1d5dfae5eb8ca2d9f
Tim Sidie <timothy.a.sidie@nasa.gov>
2013-01-22 22:01:52 +0000

    Recognition of hits, some further abstraction.

--- src/Bot.java
+++ src/Bot.java
@@ -9,13 +9,12 @@ import javax.imageio.ImageIO;
 
 public class Bot {
 
-	public static void main(String[] args) throws AWTException,
-			InterruptedException {
-		int i = 0;
+	public static void main(String[] args) throws AWTException, InterruptedException {
 		int x = 0;
 		int y = 0;
+		int xhit = 0;
 		int color = 0;
-		int WHITE = 240;
+		int WHITE = 245;
 
 		int TOP_LEFT_X = 874;
 		int TOP_LEFT_Y = 550;
@@ -25,6 +24,12 @@ public class Bot {
 		int HR_ZONE = 105;
 		int SAFE_ZONE = 50;
 
+		int BASELINE_Y = 150;
+		
+		// adjust this for each pitcher
+		int PITCHES = 10;
+		int hits = 0;
+		
 		boolean once = false;
 		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
 		GraphicsDevice devices[] = ge.getScreenDevices();
@@ -34,43 +39,35 @@ public class Bot {
 		Rectangle screenRect = new Rectangle(TOP_LEFT_X, TOP_LEFT_Y,
 				WIDTH_OF_AREA, HEIGHT_OF_AREA);
 		
-		System.out.println(InputEvent.BUTTON1_MASK);
+		//System.out.println(InputEvent.BUTTON1_MASK);
 		
-		while (i < 100000) {
-			BufferedImage img = robot.createScreenCapture(screenRect);
+		while (hits < PITCHES) {
+			
+			BufferedImage screen = robot.createScreenCapture(screenRect);
+			outerloop:
 			for (x = 0; x < screenRect.width; x++) {
-				for (y = 0; y < HEIGHT_OF_AREA; y++) {
+				for (y = 0; y < screenRect.height; y++) {
 					
-					color = img.getRGB(x, y);
-
+					color = screen.getRGB(x, y);
+					
+					//red component is shifted 16 bits
+					//green component is shifted 8 bits
+					//blue component is simply and'ed
 					if ((color & 0xFF) > WHITE && (color >> 8 & 0xFF) > WHITE
-							&& (color >> 16 & 0xFF) > WHITE) {
+							&& (color >> 16 & 0xFF) > WHITE) 
+					{
+						
 						// keep Y centered so that Pooh only moves left to
 						// right.
-						robot.mouseMove(x + TOP_LEFT_X, TOP_LEFT_Y + 150);
+						if(!once)
+							robot.mouseMove(x + TOP_LEFT_X, TOP_LEFT_Y + BASELINE_Y);
 
-						i++;
 						if (y > HR_ZONE) {
 							robot.mousePress(InputEvent.BUTTON1_MASK);
-
-							if (!once) {
-								// it turns out robot.delay can be used instead
-								// of sleeping all threads.
-								// also, look into autodelay, which is a
-								// component of robot instances
-								// robot.delay(500);
-								Set<Thread> threadSet = Thread
-										.getAllStackTraces().keySet();
-								Thread[] threadArray = threadSet
-										.toArray(new Thread[threadSet.size()]);
-								for (final Thread th : threadArray) {
-									System.out.println("Thread " + th.getId()
-											+ " sleeping.");
-									Thread.sleep(500);
-								}
+							xhit = x;
+							
+							if (!once) {					
 								once = true;
-								robot.mouseMove(TOP_LEFT_X + 74,
-										TOP_LEFT_Y + 150);
 							}
 						}
 						if (y < SAFE_ZONE) {
@@ -81,19 +78,72 @@ public class Bot {
 						// Uncomment below for image generation of homerun zone
 						// - i.e. where bot tracks ball
 
-						if (i % 100 == 0) {
-							File outputfile = new File("saved" + i / 100
-									+ ".png");
-							try {
-								ImageIO.write(img, "png", outputfile);
+						//if (i % 1000 == 0) {
+							//File outputfile = new File("saved" + i / 1000
+							//		+ ".png");
+							//try {
+							//	ImageIO.write(screen, "png", outputfile);
 
-							} catch (final IOException e) {
-								e.printStackTrace();
-							}
-						}
+							//} catch (final IOException e) {
+							//	e.printStackTrace();
+							//}
+						//}
+					}
+					else if ((closeTo((color & 0xFF), 255)) && (closeTo((color >> 8 & 0xFF), 90))
+							&& (closeTo((color >> 16 & 0xFF), 25)))
+					{
+						System.out.println("Strike!");
+						robot.mouseMove(xhit/2 + TOP_LEFT_X, TOP_LEFT_Y + BASELINE_Y);
+						allThreadSleep();
+						hits++;
+						break outerloop;
+					}
+					else if ((closeTo((color & 0xFF), 83)) && (closeTo((color >> 8 & 0xFF), 130))
+							&& (closeTo((color >> 16 & 0xFF), 255)))
+					{
+						System.out.println("Home run!");
+						robot.mouseMove(xhit/2 + TOP_LEFT_X, TOP_LEFT_Y + BASELINE_Y);
+						allThreadSleep();
+						hits++;
+						break outerloop;
+					}
+					else if ((closeTo((color & 0xFF), 57)) && (closeTo((color >> 8 & 0xFF), 175))
+							&& (closeTo((color >> 16 & 0xFF), 255)))
+					{
+						System.out.println("Hit!");
+						robot.mouseMove(xhit/2 + TOP_LEFT_X, TOP_LEFT_Y + BASELINE_Y);
+						allThreadSleep();
+						hits++;
+						break outerloop;
 					}
 				}
 			}
 		}
 	}
+	
+	public static boolean closeTo(int num1, int num2)
+	{
+		if((num1 <= num2 + 1) && (num1 >= num2 - 1))
+			return true;
+		else
+			return false;
+				
+	}
+	
+	// it turns out robot.delay can be used instead
+	// of sleeping all threads.
+	// also, look into autodelay, which is a
+	// component of robot instances
+	// robot.delay(500);
+	public static void allThreadSleep() throws InterruptedException
+	{
+		Set<Thread> threadSet = Thread.getAllStackTraces().keySet();
+		Thread[] threadArray = threadSet.toArray(new Thread[threadSet.size()]);
+		for (Thread th : threadArray) {
+			//System.out.println("Thread " + th.getId() + " sleeping.");
+			th.sleep(400);
+		}
+				
+	}
 }
+
