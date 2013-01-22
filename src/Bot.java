9a7d4829795af125687540249e197c2873cccec3
Tim Sidie <timothy.a.sidie@nasa.gov>
2013-01-22 23:39:33 +0000

    Fixed issue where Lumpy would cause bot to freeze.

--- src/Bot.java
+++ src/Bot.java
@@ -10,29 +10,30 @@ import javax.imageio.ImageIO;
 public class Bot {
 
 	public static void main(String[] args) throws AWTException, InterruptedException {
+		int i = 0;
 		int x = 0;
 		int y = 0;
-		int xhit = 0;
 		int color = 0;
-		int WHITE = 245;
+		int WHITE = 250;
 
-		int TOP_LEFT_X = 874;
-		int TOP_LEFT_Y = 550;
+		int TOP_LEFT_X = 870;
+		int TOP_LEFT_Y = 530;
 		int HEIGHT_OF_AREA = 250;
 		int WIDTH_OF_AREA = 160;
 
 		//decrease HR_ZONE for faster pitches
 		//increase it for slower pitches
-		//100 is best for first pitcher, eeyore
-		//90 for dumbo
-		//80 works well for piglet
-		int HR_ZONE = 100;
+		//120 for 1st pitcher
+		//110 for 2nd pitcher
+		//90 for 3rd pitcher
+		
+		int HR_ZONE = 110;
 		int SAFE_ZONE = 50;
 
-		int BASELINE_Y = 150;
+		int BASELINE_Y = 200;
 		
 		// adjust this for each pitcher
-		int PITCHES = 10;
+		int PITCHES = 15;
 		int hits = 0;
 		
 		boolean once = false;
@@ -40,18 +41,15 @@ public class Bot {
 		GraphicsDevice devices[] = ge.getScreenDevices();
 
 		Robot robot = new Robot(devices[1]);
-		robot.mouseMove(1200, 800);
 		Rectangle screenRect = new Rectangle(TOP_LEFT_X, TOP_LEFT_Y,
 				WIDTH_OF_AREA, HEIGHT_OF_AREA);
-		
-		//System.out.println(InputEvent.BUTTON1_MASK);
 		
 		while (hits < PITCHES) {
 			
 			BufferedImage screen = robot.createScreenCapture(screenRect);
 			outsideloop:
-			for (x = 0; x < screenRect.width; x++) {
-				for (y = 0; y < screenRect.height; y++) {
+			for (x = 0; x < screenRect.width; x=x+1) {
+				for (y = 0; y < screenRect.height; y=y+1) {
 					
 					color = screen.getRGB(x, y);
 					
@@ -65,38 +63,44 @@ public class Bot {
 						// keep Y centered so that Pooh only moves left to
 						// right.
 						if(!once)
+						{
 							robot.mouseMove(x + TOP_LEFT_X, TOP_LEFT_Y + BASELINE_Y);
 
-						if (y > HR_ZONE) {
-							robot.mousePress(InputEvent.BUTTON1_MASK);
-							xhit = x;
-							
-							if (!once) {					
-								once = true;
+							if (y > HR_ZONE) {
+								if (!once) {					
+									once = true;
+								}
+								robot.mousePress(InputEvent.BUTTON1_MASK);
 							}
+							
+							
 						}
-						if (y < SAFE_ZONE) {
-							robot.mouseRelease(InputEvent.BUTTON1_MASK);
-							once = false;
+						if(once)
+						{
+							if (y < SAFE_ZONE) {
+								robot.mouseRelease(InputEvent.BUTTON1_MASK);
+								once = false;
+							}
 						}
 
 						// Uncomment below for image generation of homerun zone
 						// - i.e. where bot tracks ball
 
 						//if (i % 1000 == 0) {
-							//File outputfile = new File("saved" + i / 1000
-							//		+ ".png");
-							//try {
-							//	ImageIO.write(screen, "png", outputfile);
+						//	File outputfile = new File("saved" + i / 1000
+						//			+ ".png");
+						//	try {
+						//		ImageIO.write(screen, "png", outputfile);
 
-							//} catch (final IOException e) {
-							//	e.printStackTrace();
-							//}
+						//	} catch (final IOException e) {
+						//		e.printStackTrace();
+						//	}
 						//}
 					}
 					else if ((closeTo((color & 0xFF), 255)) && (closeTo((color >> 8 & 0xFF), 90))
 							&& (closeTo((color >> 16 & 0xFF), 25)))
 					{
+						once = false;
 						robot.mouseRelease(InputEvent.BUTTON1_MASK);
 						System.out.println("Strike!");
 						robot.mouseMove(WIDTH_OF_AREA/2 + TOP_LEFT_X, TOP_LEFT_Y + BASELINE_Y);
@@ -107,6 +111,7 @@ public class Bot {
 					else if ((closeTo((color & 0xFF), 83)) && (closeTo((color >> 8 & 0xFF), 130))
 							&& (closeTo((color >> 16 & 0xFF), 255)))
 					{
+						once = false;
 						robot.mouseRelease(InputEvent.BUTTON1_MASK);
 						System.out.println("Home run!");
 						robot.mouseMove(WIDTH_OF_AREA/2 + TOP_LEFT_X, TOP_LEFT_Y + BASELINE_Y);
@@ -117,12 +122,19 @@ public class Bot {
 					else if ((closeTo((color & 0xFF), 57)) && (closeTo((color >> 8 & 0xFF), 175))
 							&& (closeTo((color >> 16 & 0xFF), 255)))
 					{
+						once = false;
 						robot.mouseRelease(InputEvent.BUTTON1_MASK);
 						System.out.println("Hit!");
 						robot.mouseMove(WIDTH_OF_AREA/2 + TOP_LEFT_X, TOP_LEFT_Y + BASELINE_Y);
 						allThreadSleep();
 						hits++;
 						break outsideloop;
+					}
+					//end screen
+					else if ((closeTo((color & 0xFF), 107)) && (closeTo((color >> 8 & 0xFF), 213))
+							&& (closeTo((color >> 16 & 0xFF), 250)))
+					{
+						System.exit(1);
 					}
 					else
 					{
