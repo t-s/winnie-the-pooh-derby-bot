ad718bc60228fdfb166f6d80d808721f989daa59
Tim Sidie <timothy.a.sidie@nasa.gov>
2013-01-28 22:57:39 +0000

    Added initial mouse click to determine where Flash app is.

--- src/Bot.java
+++ src/Bot.java
@@ -6,18 +6,30 @@ import java.io.IOException;
 import java.util.Set;
 
 import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+
+import java.awt.event.AWTEventListener;
 
 public class Bot {
 
+	public static int TOP_LEFT_X;
+	public static int TOP_LEFT_Y;
+	public static boolean coordsSet = false;
+
 	public static void main(String[] args) throws AWTException, InterruptedException {
+		
+		Toolkit.getDefaultToolkit().addAWTEventListener(
+				new Listener(), AWTEvent.MOUSE_EVENT_MASK | AWTEvent.FOCUS_EVENT_MASK);
+	    JFrame frame = new JFrame();
+	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    frame.setVisible(true);
+
 		int i = 0;
 		int x = 0;
 		int y = 0;
 		int color = 0;
 		int WHITE = 250;
 
-		int TOP_LEFT_X = 870;
-		int TOP_LEFT_Y = 530;
 		int HEIGHT_OF_AREA = 250;
 		int WIDTH_OF_AREA = 160;
 
@@ -26,26 +38,36 @@ public class Bot {
 		//120 for 1st pitcher
 		//110 for 2nd pitcher
 		//90 for 3rd pitcher
-		
 		//105 for 5th pitcher
 		
-		int HR_ZONE = 55;
+		int HR_ZONE = 120;
 		int SAFE_ZONE = 20;
 
 		int BASELINE_Y = 200;
 		
 		// adjust this for each pitcher
-		int PITCHES = 30;
+		int PITCHES = 10;
 		int hits = 0;
-		boolean ballvisible = false;
-		
+		boolean ballvisible = true;
+		boolean pitched = false;
 		boolean once = false;
 		
 		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
 		GraphicsDevice devices[] = ge.getScreenDevices();
-
-		Robot robot = new Robot(devices[1]);
-		Rectangle screenRect = new Rectangle(TOP_LEFT_X, TOP_LEFT_Y,
+		Robot robot = new Robot(devices[devices.length-1]);
+		
+		while (!coordsSet)
+		{
+			//this loop will run forever, regardless of whether or not mouse has been clicked
+			//unless this print statement runs:
+			//I will have to see why that is!
+			System.out.println(coordsSet);
+		}
+		
+		System.out.println("X is now " + TOP_LEFT_X);
+		System.out.println("Y is now " + TOP_LEFT_Y);
+		
+		Rectangle screenRect = new Rectangle(Bot.TOP_LEFT_X, TOP_LEFT_Y,
 				WIDTH_OF_AREA, HEIGHT_OF_AREA);
 		
 		while (hits < PITCHES) {
@@ -63,12 +85,14 @@ public class Bot {
 					if (((color & 0xFF) > WHITE) && ((color >> 8 & 0xFF) > WHITE)
 							&& ((color >> 16 & 0xFF) > WHITE)) 
 					{
-						
+
 						// keep Y centered so that Pooh only moves left to
 						// right.
 						if(!once)
 						{
 							robot.mouseMove(x + TOP_LEFT_X, TOP_LEFT_Y + BASELINE_Y);
+							if(!ballvisible)
+								ballvisible = true;
 							i++;
 
 							if (y >= HR_ZONE) {
@@ -76,11 +100,7 @@ public class Bot {
 									once = true;
 								}
 								robot.mousePress(InputEvent.BUTTON1_MASK);
-								ballvisible = false;
 							}
-							
-							if(!ballvisible)
-								ballvisible = true;
 							
 						}
 						if(once)
@@ -94,7 +114,7 @@ public class Bot {
 						// Uncomment below for image generation of homerun zone
 						// - i.e. where bot tracks ball
 
-						if (i % 100 == 0) {
+						if (i % 10 == 0) {
 							File outputfile = new File("saved" + i / 100
 									+ ".png");
 							try {
@@ -114,6 +134,7 @@ public class Bot {
 						robot.mouseMove(WIDTH_OF_AREA/2 + TOP_LEFT_X, TOP_LEFT_Y + BASELINE_Y);
 						allThreadSleep();
 						hits++;
+						
 						break outsideloop;
 					}
 					else if ((closeTo((color & 0xFF), 83)) && (closeTo((color >> 8 & 0xFF), 130))
@@ -125,6 +146,7 @@ public class Bot {
 						robot.mouseMove(WIDTH_OF_AREA/2 + TOP_LEFT_X, TOP_LEFT_Y + BASELINE_Y);
 						allThreadSleep();
 						hits++;
+						
 						break outsideloop;
 					}
 					else if ((closeTo((color & 0xFF), 57)) && (closeTo((color >> 8 & 0xFF), 175))
@@ -136,6 +158,7 @@ public class Bot {
 						robot.mouseMove(WIDTH_OF_AREA/2 + TOP_LEFT_X, TOP_LEFT_Y + BASELINE_Y);
 						allThreadSleep();
 						hits++;
+						
 						break outsideloop;
 					}
 					//end screen
@@ -148,10 +171,11 @@ public class Bot {
 					{
 						if(once)
 							once = false;
+						
 						if(ballvisible)
 						{
-							
-								
+							ballvisible = false;
+							//System.out.println("Ball has become invisible!");	
 						}
 					}
 				}
@@ -168,11 +192,6 @@ public class Bot {
 				
 	}
 	
-	// it turns out robot.delay can be used instead
-	// of sleeping all threads.
-	// also, look into autodelay, which is a
-	// component of robot instances
-	// robot.delay(500);
 	public static void allThreadSleep() throws InterruptedException
 	{
 		Set<Thread> threadSet = Thread.getAllStackTraces().keySet();
@@ -183,4 +202,21 @@ public class Bot {
 		}
 				
 	}
+	
+	static class Listener extends Bot implements AWTEventListener {
+		static boolean enabled = true;
+        public void eventDispatched(AWTEvent event) {
+        	
+        	if(event.getID() == 1005)
+        	{
+        		if(enabled == true)
+        		{
+        			TOP_LEFT_X = MouseInfo.getPointerInfo().getLocation().x + 240;
+        			TOP_LEFT_Y = MouseInfo.getPointerInfo().getLocation().y + 90;
+        			enabled = false;
+        			coordsSet = true;
+        		}
+        	}
+        }
+    }
 }
